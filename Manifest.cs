ec8b55047402ecbbf46601c0ce2ca38cd99f024a
Peter Keating <pete.keating@etchuk.com>
2020-12-07 21:14:23 +0000

    v0.3.2

--- Manifest.cs
+++ Manifest.cs
@@ -5,6 +5,6 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Widget for displaying edit control for page being viewed.",
     Name = "Contextual Edit",
-    Version = "0.3.1",
+    Version = "0.3.2",
     Website = "https://etchuk.com"
 )]
