951ba168ee9a882d44729d3d24345ec11aa34fbd
Peter Keating <pete.keating@etchuk.com>
2023-01-06 09:09:57 +0000

    Use git tag name to populate version number during deployment

--- Manifest.cs
+++ Manifest.cs
@@ -5,6 +5,6 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Widget for displaying edit control for page being viewed.",
     Name = "Contextual Edit",
-    Version = "1.4.0",
+    Version = "$(VersionNumber)",
     Website = "https://etchuk.com"
 )]
